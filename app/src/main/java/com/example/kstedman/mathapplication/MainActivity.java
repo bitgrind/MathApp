05dee34dc9c898a6b077f0ba86e547ca07f3af56
Keith Stedman <keithstedman@gmail.com>
2017-05-28 12:17:43 -0700

    added button with toast now making new solve view

--- app/src/main/java/com/example/kstedman/mathapplication/MainActivity.java
+++ app/src/main/java/com/example/kstedman/mathapplication/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.kstedman.mathapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class cd MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+    private Button mSolveEquationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSolveEquationButton = (Button) findViewById(R.id.solveEquationButton);
+
+        mSolveEquationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
