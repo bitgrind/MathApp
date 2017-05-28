abfd7b94558f15b43760cdc341b86f158609c990
Keith Stedman <keithstedman@gmail.com>
2017-05-28 12:29:12 -0700

    added new view

--- app/src/main/java/com/example/kstedman/mathapplication/MainActivity.java
+++ app/src/main/java/com/example/kstedman/mathapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kstedman.mathapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,13 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mSolveEquationButton = (Button) findViewById(R.id.solveEquationButton);
 
         mSolveEquationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, SolveActivity.class);
+                startActivity(intent);
             }
         });
     }
