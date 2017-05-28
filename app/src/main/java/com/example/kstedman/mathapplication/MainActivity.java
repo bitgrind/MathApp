02ef72cf4eae8a4ed1ee991c3411018407cc9576
Keith Stedman <keithstedman@gmail.com>
2017-05-28 15:45:30 -0700

    passing information from main view to solve view

--- app/src/main/java/com/example/kstedman/mathapplication/MainActivity.java
+++ app/src/main/java/com/example/kstedman/mathapplication/MainActivity.java
@@ -3,23 +3,32 @@ package com.example.kstedman.mathapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mSolveEquationButton;
+    private EditText mInputEquation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSolveEquationButton = (Button) findViewById(R.id.solveEquationButton);
+        mInputEquation = (EditText) findViewById(R.id.inputEquation);
+
 
         mSolveEquationButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
+                String mathEquation = mInputEquation.getText().toString();
+                Log.d("Input Equation", "Whats up");
                 Intent intent = new Intent(MainActivity.this, SolveActivity.class);
+                intent.putExtra("equation1", mathEquation);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/kstedman/mathapplication/SolveActivity.java
+++ app/src/main/java/com/example/kstedman/mathapplication/SolveActivity.java
@@ -1,13 +1,20 @@
 package com.example.kstedman.mathapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SolveActivity extends AppCompatActivity {
+    private TextView mEquationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solve);
+        mEquationText = (TextView) findViewById(R.id.inputEquation);
+        Intent intent = getIntent();
+        String equation = intent.getStringExtra("equation1");
+        mEquationText.setText("equation input"+equation);
     }
 }
