e5353cc9e564f6bf1a6135b071461e27a406cb31
Keith Stedman <keithstedman@gmail.com>
2017-05-29 10:25:31 -0700

    added navigation to the main activity

--- app/src/main/java/com/example/kstedman/mathapplication/MainActivity.java
+++ app/src/main/java/com/example/kstedman/mathapplication/MainActivity.java
@@ -11,6 +11,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mSolveEquationButton;
+    private Button mSolveButton;
+    private Button mAboutButton;
+    private Button mContactButton;
+    private Button mConvertButton;
     private EditText mInputEquation;
 
     @Override
@@ -18,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSolveEquationButton = (Button) findViewById(R.id.solveEquationButton);
+        mAboutButton = (Button) findViewById(R.id.aboutButton);
+        mConvertButton = (Button) findViewById(R.id.convertButton);
+        mContactButton = (Button) findViewById(R.id.contactbutton);
+        mSolveButton = (Button) findViewById(R.id.solveButton);
         mInputEquation = (EditText) findViewById(R.id.inputEquation);
 
 
@@ -29,6 +37,38 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Input Equation", "Whats up");
                 Intent intent = new Intent(MainActivity.this, SolveActivity.class);
                 intent.putExtra("equation1", mathEquation);
+                startActivity(intent);
+            }
+        });
+
+        mAboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mContactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContactActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mConvertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ConvertActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mSolveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
