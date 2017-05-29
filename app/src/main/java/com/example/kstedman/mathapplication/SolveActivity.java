b06f8cd915c1262c8628eea52b35e2c9f2658b38
Keith Stedman <keithstedman@gmail.com>
2017-05-28 17:02:48 -0700

    added list array for equation steps

--- app/src/main/java/com/example/kstedman/mathapplication/SolveActivity.java
+++ app/src/main/java/com/example/kstedman/mathapplication/SolveActivity.java
@@ -3,18 +3,29 @@ package com.example.kstedman.mathapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class SolveActivity extends AppCompatActivity {
     private TextView mEquationText;
+    private ListView mListView;
+    private String[] equationSteps = new String[] {"This is the complicated Problem","This is the less complicated problem","More Less complicated","the most simple"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solve);
+
+        mListView = (ListView) findViewById(R.id.equationSteps);
         mEquationText = (TextView) findViewById(R.id.inputEquation);
+
         Intent intent = getIntent();
         String equation = intent.getStringExtra("equation1");
         mEquationText.setText(equation);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, equationSteps);
+        mListView.setAdapter(adapter);
+
     }
 }
